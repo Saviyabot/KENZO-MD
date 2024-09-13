3d5e250aab577812e2589b22306cdb87b73fd168
SENESH-KEVIN <Nadeeshalakmal1981@gmail.com>
2024-09-13 15:01:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `Wa-BOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/3c64b5608dd82d33dabe8.jpg` }, caption: up })
 
 }
 })
