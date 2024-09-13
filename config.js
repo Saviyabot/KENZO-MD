22035c8e71ebc2d871c56fb08caf1397b05e09f2
SENESH-KEVIN <Nadeeshalakmal1981@gmail.com>
2024-09-13 14:57:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "xSkUkIwD#xnHDi1lZl8GgFTlkOUQklL811mNy0igBAEN3UEGRAAo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3c64b5608dd82d33dabe8.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Kevin-Md i am alive now..!",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true" false,
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Kenzo-Md i am alive now..!",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public"
 };
