eeeaea0fc6ecd360c3fee5995b8b555790f8a0c1
SENESH-KEVIN <Nadeeshalakmal1981@gmail.com>
2024-09-13 23:13:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,4 +10,5 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3c64b5608dd82d33dab
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Kenzo-Md i am alive now..!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
+PREFIX: process.env.PREFIX || "."
 };
